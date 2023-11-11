1e9ce5dea4445e3a986dc463c742cb773a262ac4
Wang Lun <me@wanglun.org>
2023-11-11 19:36:51 +0800

    Rename keymap

--- plugin/ai.lua
+++ plugin/ai.lua
@@ -13,7 +13,7 @@ end, {
 })
 
 if not vim.g.ai_no_mappings then
-    vim.api.nvim_set_keymap("n", "<C-a>", ":Chat ", { noremap = true })
-    vim.api.nvim_set_keymap("v", "<C-a>", ":Chat ", { noremap = true })
-    vim.api.nvim_set_keymap("i", "<C-a>", "<Esc>:Chat<CR>a", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<C-a>", ":AI ", { noremap = true })
+    vim.api.nvim_set_keymap("v", "<C-a>", ":AI ", { noremap = true })
+    vim.api.nvim_set_keymap("i", "<C-a>", "<Esc>:AI<CR>a", { noremap = true })
 end
